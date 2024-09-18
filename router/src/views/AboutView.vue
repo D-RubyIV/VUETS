60699db8f03e1617c6dcee657f5899cda6dd306c
04 DION <phamhaanh2k4.php@gmail.com>
2024-09-18 17:56:10 +0700

    200

--- router/src/views/AboutView.vue
+++ router/src/views/AboutView.vue
@@ -1,6 +1,31 @@
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter();
+
+// Hàm chuyển hướng với params
+const pushWithParams = () => {
+  router.push({ name: 'update', params: { id: 123, age: 67 } });
+};
+
+// Hàm chuyển hướng với query
+const pushWithQuery = () => {
+  router.push({
+    name: 'update',  // Đảm bảo rằng route của bạn có name là 'Customer'
+    params: { id: 123, age: 67 },
+    query: { search: 'Vue' }
+  });
+};
+
+
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <button @click="pushWithParams">Go to User with Params</button>
+    <button @click="pushWithQuery">Go to User with Query</button>
   </div>
 </template>
 
--- router/src/views/DetailView.vue
+++ router/src/views/DetailView.vue
@@ -4,5 +4,9 @@
   </div>
 </template>
 <script lang="ts" setup>
-
+import { useRoute } from 'vue-router'
+import { onBeforeMount, onMounted } from 'vue'
+const route = useRoute();
+console.log(route.params.id, route.params.age); // Lấy giá trị param 'userId'
+console.log(route.params.id, route.params.age, route.query); // Lấy giá trị param 'userId'
 </script>
